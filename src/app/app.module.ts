5b66a3f6ccbb977ab276544647a56976f77e98ca
Santiago Muñoz <sorduz98@outlook.com>
2022-01-06 17:45:12 -0500

    Added table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { UserComponent } from './pages/user/user.component';
 import { UsersComponent } from './pages/users/users.component';
-import { FooterComponent } from './components/footer/footer.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserComponent,
     UsersComponent,
-    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private _users = new BehaviorSubject<any>([]);
+
+  private data_users = [
+    {
+      fullname: 'John Doe',
+      birthdate: new Date(),
+      identification: 13233445566,
+    },
+    {
+      fullname: 'John Doe',
+      birthdate: new Date(),
+      identification: 13233445566,
+    },
+    {
+      fullname: 'John Doe',
+      birthdate: new Date(),
+      identification: 13233445566,
+    }
+  ]
+  constructor(private http: HttpClient) {
+    this._users.next(this.data_users);
+  }
+
+  get users() {
+    return this._users.asObservable();
+  }
+
+  getUsers() {
+    return this.http.get('https://jsonplaceholder.typicode.com/users');
+  }
+
+  createUser() {
+    return this.http.post('https://jsonplaceholder.typicode.com/users', {});
+  }
+}
