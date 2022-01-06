7a20f893ab32387f889d6adc66034df6df1c826b
Santiago Muñoz <sorduz98@outlook.com>
2022-01-06 18:24:21 -0500

    Added user form CSS

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, of, Subject, take, takeUntil } from 'rxjs';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, OnDestroy {
+  public users: Observable<any> = of([]);
+  private destroy$ = new Subject<void>();
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.getUsers()
+      .pipe(take(1), takeUntil(this.destroy$))
+      .subscribe(data => {
+        // console.log(data);
+      });
+
+    this.users = this.userService.users;
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+    this.destroy$.unsubscribe();
   }
 
 }
