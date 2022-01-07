423f7693d1d80bf2a2bbf58699ec6e37aaf2abc4
Santiago Muñoz <sorduz98@outlook.com>
2022-01-06 23:42:28 -0500

    Added retry to avoid CORS

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, tap } from 'rxjs';
+import { BehaviorSubject, delay, retryWhen, take, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -20,6 +20,7 @@ export class UserService {
     // https://jsonplaceholder.typicode.com/
     return this.http.get<any[]>(`${environment.apiUrl}users`)
       .pipe(
+        retryWhen(errors => errors.pipe(delay(1000), take(3))),
         tap(
           (users) => {
             users.sort((a, b) => a?.id - b?.id);
@@ -36,6 +37,8 @@ export class UserService {
       father_id: null,
       mother_id: null,
     };
-    return this.http.post(`${environment.apiUrl}users`, payload);
+    return this.http.post(`${environment.apiUrl}users`, payload).pipe(
+      retryWhen(errors => errors.pipe(delay(1000), take(3))),
+    );
   }
 }
