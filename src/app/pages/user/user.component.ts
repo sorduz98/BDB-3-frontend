15b1b90f1b126ed0deaac9f79ea9206fea0f78f5
Santiago Muñoz <sorduz98@outlook.com>
2022-01-06 19:04:54 -0500

    Added reactive form

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  public userForm: FormGroup = this.fb.group({
+      fullname: ['', [Validators.required, Validators.minLength(10)]],
+      identification: ['', [Validators.required, Validators.min(10000)]],
+      birthdate: ['', [Validators.required]],
+    },
+    { updateOn: 'submit' }
+  );
 
-  constructor() { }
+  public loading = false;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.userForm.get('fullname')?.dirty
+  }
+
+  onSubmit() {
+    this.userForm.markAllAsTouched();
+    console.log(this.userForm.value);
   }
 
 }
